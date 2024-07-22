170ace6a2c5ba6acbeb6c83d9fca501e09fdbc74
Sandeep Vashishtha <sandeepvashishtha@outlook.in>
2024-07-23 00:25:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 import CodeEditor from './CodeEditor';
 
 function App() {
-    return (
-        <div className="App">
-            <CodeEditor />
-        </div>
-    );
+  return (
+    <div className="App">
+      <CodeEditor />
+    </div>
+  );
 }
 
 export default App;
